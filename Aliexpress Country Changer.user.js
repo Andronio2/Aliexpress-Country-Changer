7215067bafd0e7e84878d80a6860c1d4676e68b6
Andronio2 <42270003+Andronio2@users.noreply.github.com>
2020-12-11 11:50:49 +0600

    Первая загрузка файла

---
+++ Aliexpress Country Changer.user.js
@@ -0,0 +1,92 @@
+// ==UserScript==
+// @name         Aliexpress Country Changer
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Изменяет страну и валюту
+// @author       Andronio
+// @match        https://*.aliexpress.ru/*
+// @match        https://*.aliexpress.com/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+(function () {
+    'use strict';
+
+let myCurrency	= "KZT";
+let myCountry	= "KZ";
+let myLocale    = "ru_RU";
+
+/*    const cookieSettings = {
+		'aep_usuc_f': {
+			'region':   myCountry,
+			'b_locale': myLocale,
+			'c_tp':     myCurrency,
+		},
+		'xman_us_f': {
+			'x_locale': myLocale
+		}
+	}
+*/
+
+/*    const cookieSettings = {
+		'aep_usuc_f': {
+			'region':   myCountry,
+			'c_tp':     myCurrency,
+			'alimid':	'2'
+		}
+	}
+*/
+    const cookieSettings = {
+		'aep_usuc_f': {
+			'region':   myCountry,
+			'c_tp':     myCurrency
+		}
+	}
+	
+	if (!location.href.includes("mydata=123")) {
+		let strMass = [];
+		let cookies = document.cookie.split('; ');
+		let isNeedModif = false;
+
+		const findNReplace = (str, key, value) => {
+			if (str.includes(key)) {
+				let re = new RegExp(key + '=\\w{' + value.length + '}');
+                if (str.match(re)[0] !== (key + '=' + value)) {
+                    str = str.replace(re, key + '=' + value);
+                    isNeedModif = true;
+                }
+            } else {
+                str = key + '=' + value + str;
+                isNeedModif = true;
+            }
+			return str;
+		};
+
+        const setCookies = (mass, host) => {
+            mass.forEach( str => {
+				document.cookie = `${str}; path=/; expires=Tue, 19 Jan 2088 03:14:07 GMT; domain=.${host}`;
+
+            });
+        };
+
+		cookies.forEach(cookie => {
+			for (let cookieName in cookieSettings) {
+                if (cookie.startsWith(cookieName)) {                                    // Если нашел нужную куку
+                    let str = cookie.slice(cookieName.length + 1)                       // Убрали начало
+                    for (let subCookie in cookieSettings[cookieName]) {
+                        str = findNReplace(str, subCookie, cookieSettings[cookieName][subCookie]);
+                    }
+                    strMass.push(cookieName + '=' + str);
+                }
+            }
+		});
+		if (isNeedModif) {
+			if (location.hostname.includes("aliexpress.ru")) {
+                setCookies(strMass, "aliexpress.ru");
+			} else if (location.hostname.includes("aliexpress.com")) {
+                setCookies(strMass, "aliexpress.com");
+			} else {alert("Ошибка в скрипте")};
+            location.reload();
+        };
+	};
+})();
